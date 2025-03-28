d85113af012540863dfb1845de945df9b918d47e
Simone Finessi <simone.finessi.06@gmail.com>
2025-03-28 13:52:07 +0100

    punto7

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/rest', usersRouter);
 app.use('/pages', pagesRouter);
+
+app.use(function(req, res, next) {
+  if (req.path.startsWith('/rest') && !req.path.startsWith('/pages')) {
+    return next(createError(404, 'Route not found'));
+  }
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
