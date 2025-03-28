fc0eef2350254ceb972d777b07030fdde30a6fc5
Simone Finessi <simone.finessi.06@gmail.com>
2025-03-28 13:45:04 +0100

    punto 5

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+const people = require('../persone.json');
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
-  const people = require('../persone.json');
+  
   res.send(people);
 });
+router.get('/users/:email', function(req, res, next) {
+  const persona = people.find(P => P.email === req.params.email);
+  res.send(persona);
 
+});
 module.exports = router;
